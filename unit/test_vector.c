cd363ce8cccd17b69e02a10ccc7ecd1c53492118
Florian Märkl <info@florianmaerkl.de>
2018-07-30 16:59:52 +0200

    Remove old vector unit tests

--- unit/test_vector.c
+++ unit/test_vector.c
@@ -48,101 +48,6 @@ static void init_test_pvector2(RPVector *v, size_t len, size_t padding) {
 	}
 }
 
-static bool test_r_vector_old() {
-	ptrdiff_t i;
-	void **it;
-	void *val;
-	RPVector *v = r_pvector_new (NULL);
-	it = r_pvector_push (v, (void *)1337);
-	mu_assert_eq_fmt (*it, (void *)1337, "first push", "%p");
-	for (i = 0; i < 10; i++) {
-		r_pvector_push (v, (void *)i);
-		mu_assert_eq_fmt (v->v.len, i + 2, "push more", "%lu");
-	}
-	val = r_pvector_pop_front (v);
-	mu_assert_eq_fmt (val, (void *)1337, "pop_front", "%p");
-	mu_assert ("contains", r_pvector_contains (v, (void *)9));
-
-	val = r_pvector_remove_at (v, 9);
-	mu_assert_eq_fmt (val, (void *)9, "remove_at", "%p");
-	mu_assert ("!contains after remove_at", !r_pvector_contains (v, (void *)9));
-
-	i = 0;
-	r_pvector_foreach (v, it) {
-		void *e = (void *)i++;
-		mu_assert_eq_fmt (*it, e, "remaining elements", "%p");
-	}
-
-	r_pvector_shrink (v);
-	mu_assert_eq_fmt (v->v.len, 9UL, "len after shrink", "%lu");
-	//RPVector *v1 = r_pvector_clone (v);
-	r_pvector_clear (v);
-	mu_assert ("empty after clear", v->v.capacity == 0 && v->v.len == 0);
-	//mu_assert_eq_fmt (v1->v.len, 9UL, "unaffected source after clone", "%lu");
-
-	r_pvector_free (v);
-	//r_pvector_free (v1);
-
-	RPVector s;
-	r_pvector_init (&s, NULL);
-	r_pvector_reserve (&s, 10);
-	r_pvector_clear (&s);
-
-	r_pvector_reserve (&s, 10);
-	r_pvector_push (&s, (void *)-1);
-	mu_assert ("stack allocated vector init", s.v.len == 1 && s.v.capacity == 10);
-	for (i = 0; i < 20; i++) {
-		r_pvector_push (&s, (void *)i);
-	}
-	r_pvector_reserve (&s, 10);
-	r_pvector_clear (&s);
-
-	{
-		void *a[] = {(void*)0, (void*)2, (void*)4, (void*)6, (void*)8};
-		RPVector s;
-		r_pvector_init (&s, NULL);
-		size_t l;
-		r_pvector_insert_range (&s, 0, a + 2, 3);
-		r_pvector_insert_range (&s, 0, a, 2);
-
-#define CMP(x, y) x - y
-		r_pvector_lower_bound (&s, (void *)4, l, CMP);
-		mu_assert_eq_fmt (r_pvector_at (&s, l), (void *)4, "lower_bound", "%p");
-		r_pvector_lower_bound (&s, (void *)5, l, CMP);
-		mu_assert_eq_fmt (r_pvector_at (&s, l), (void *)6, "lower_bound 2", "%p");
-		r_pvector_lower_bound (&s, (void *)6, l, CMP);
-		mu_assert_eq_fmt (r_pvector_at (&s, l), (void *)6, "lower_bound 3", "%p");
-		r_pvector_lower_bound (&s, (void *)9, l, CMP);
-		mu_assert_eq_fmt (l, s.v.len, "lower_bound 3", "%lu");
-
-		r_pvector_upper_bound (&s, (void *)4, l, CMP);
-		mu_assert_eq_fmt (r_pvector_at (&s, l), (void *)6, "upper_bound", "%p");
-		r_pvector_upper_bound (&s, (void *)5, l, CMP);
-
-		mu_assert_eq_fmt (r_pvector_at (&s, l), (void *)6, "upper_bound 2", "%p");
-		r_pvector_upper_bound (&s, (void *)6, l, CMP);
-		mu_assert_eq_fmt (r_pvector_at (&s, l), (void *)8, "upper_bound 3", "%p");
-#undef CMP
-
-		r_pvector_clear (&s);
-
-		r_pvector_push (&s, strdup ("Charmander"));
-		r_pvector_push (&s, strdup ("Squirtle"));
-		r_pvector_push (&s, strdup ("Bulbasaur"));
-		r_pvector_push (&s, strdup ("Meowth"));
-		r_pvector_push (&s, strdup ("Caterpie"));
-		r_pvector_sort (&s, (RPVectorComparator) strcmp);
-
-		r_pvector_lower_bound (&s, "Meow", l, strcmp);
-		mu_assert_streq ((char *)r_pvector_at (&s, l), "Meowth", "sort, lower_bound");
-
-		s.free = free;
-		r_pvector_clear (&s);
-	}
-
-	mu_end;
-}
-
 
 static bool test_vector_init() {
 	RVector v;
@@ -1027,8 +932,6 @@ static bool test_pvector_upper_lower_bound() {
 }
 
 static int all_tests() {
-	mu_run_test (test_r_vector_old);
-
 	mu_run_test (test_vector_init);
 	mu_run_test (test_vector_new);
 	mu_run_test (test_vector_clear);
